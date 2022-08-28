119df350a7e87f8e97a3d2feb7eb929f42325d50
Ismail Courr <contact@ismailcourr.ml>
2022-08-28 12:43:12 +0100

    Clean up the page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import '../assets/css/animate.css';
+import '../assets/css/tiny-slider.css';
+import '../assets/fonts/lineicons/font-css/LineIcons.css';
+import '../assets/css/tailwindcss.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
